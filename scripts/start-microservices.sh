252660329784b4bc9431a06078238b813372c04c
dangalcan <megamagolas@gmail.com>
2025-12-06 12:04:37 +0100

    feat: add basic structure to include auth microservice in kubernetes environment

--- scripts/start-microservices.sh
+++ scripts/start-microservices.sh
@@ -1,3 +1,8 @@
+# beats interaction
 kubectl apply -f k8s/base/mongo-beats-interaction/
 kubectl apply -f k8s/base/beats-interaction/
 kubectl logs deploy/beats-interaction -f
+# user auth
+kubectl apply -f k8s/base/mongo-user-auth/
+kubectl apply -f k8s/base/redis-user-auth/
+kubectl apply -f k8s/base/user-auth/
