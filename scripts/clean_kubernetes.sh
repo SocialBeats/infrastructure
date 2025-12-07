38d16484df451081f5e3e473e848c985941ebf11
dangalcan <megamagolas@gmail.com>
2025-12-07 13:33:07 +0100

    chore: add clean_kubernetes script

---
+++ scripts/clean_kubernetes.sh
@@ -0,0 +1,9 @@
+kubectl delete all --all
+kubectl delete configmap --all
+kubectl delete secret --all
+kubectl delete pvc --all
+
+kubectl get all
+kubectl get configmap
+kubectl get secret
+kubectl get pvc
