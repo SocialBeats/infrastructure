dc4697f17602a2cb0cd52830893632a627c90a2e
Ramón Gavira <ramgavsan@alum.us.es>
2025-12-31 16:52:12 +0100

    chore: adapt to kubernetes digital ocean

--- scripts/setup/start-space.sh
+++ scripts/setup/start-space.sh
@@ -3,3 +3,14 @@ kubectl apply -f ./k8s/base/space/config/
 kubectl apply -f ./k8s/base/space/pods/
 kubectl apply -f ./k8s/base/space/load-balancer/
 kubectl get pods -n socialbeats
+
+
+
+kubectl apply -f ./k8s/base/space/config/
+kubectl apply -f ./k8s/base/space/pods/
+kubectl apply -f ./k8s/base/space/load-balancer-deployment/
+kubectl get pods -n socialbeats
+
+kubectl delete -f ./k8s/base/space/config/
+kubectl delete -f ./k8s/base/space/pods/
+kubectl delete -f ./k8s/base/space/load-balancer-deployment/
